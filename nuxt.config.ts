04885e475955e4fad2b77d98d11d31be717b62ec
pietro <pammaturo02@gmail.com>
2024-04-19 17:49:23 +0200

    First test commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Dog Shelter'
+    }
+  },
+  css: ['~/assets/css/general.css'],
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
