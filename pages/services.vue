af06eb799fa9e4359cfebeafc581a122e1601b4f
lucamarinacci <luca.marinacci@mail.polimi.it>
2024-04-29 23:36:55 +0200

    Minor changes

--- pages/services.vue
+++ pages/services.vue
@@ -5,11 +5,7 @@
 </script>
 
 <template>
-    <div class="navbar">
-        <a><NuxtLink to="/">
-            <span><h3>HOMEPAGE</h3></span>
-        </NuxtLink></a>
-    </div>
+    <Navbar></Navbar>
     <ActivitiesHeader title="Services" subtitle="Our services are ..." background="https://ckrrgcxccboxnwjognhr.supabase.co/storage/v1/object/sign/POLS/default.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJQT0xTL2RlZmF1bHQuanBnIiwiaWF0IjoxNzE0MzI0NTAyLCJleHAiOjI1NzgzMjQ1MDJ9.xqrecMdpXUDVKoI4dlCvS3EdoGWEK2ntAJY48mPYwYg&t=2024-04-28T17%3A15%3A02.994Z">
     </ActivitiesHeader>
     <ActivitiesShowcase>
