21c719e608ce8fe5674aca79ff8aef2d16b85212
pietro <pammaturo02@gmail.com>
2024-05-09 22:50:53 +0200

    Fixed icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,4 +23,7 @@ export default defineNuxtConfig({
     // do not redirect if not authenticated (not needed)
     redirect: false
   },
+  build: {
+    transpile: ['@fortawesome/vue-fontawesome']
+  },
 });
