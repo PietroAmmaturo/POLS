0ca550d95818a125797537a821566cf113c343f6
pietro <pammaturo02@gmail.com>
2024-05-09 23:23:54 +0200

    Fixed icon size flashing (too big) at startup

--- app.vue
+++ app.vue
@@ -14,4 +14,11 @@ import { fas, faPuzzlePiece, faBellConcierge, faPlus, faMinus } from '@fortaweso
 import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas, faTwitter, faFontAwesome, faPuzzlePiece, faBellConcierge, faPlus, faMinus)
+
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 </script>
